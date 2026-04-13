b71a7a9533f7e9480b3b5462d0b789c4fa788eb2
Speedy345 <mindcraftkids1976@gmail.com>
2026-04-12 21:19:39 -0600

    hopefully added Namespaces?

--- Runtime/Stats/FloatStat.cs
+++ Runtime/Stats/FloatStat.cs
@@ -3,138 +3,141 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-[Serializable]
-[InlineProperty]
-[HideDuplicateReferenceBox]
-public class FloatStat : IStat<float>
+namespace StatSystem
 {
-    public float baseValue;
-    public List<StatModifier<float>> modifiers = new();
-
-    public float cachedValue;
-    bool isDirty = true;
-    public FloatStat(float baseValue)
+    [Serializable]
+    [InlineProperty]
+    [HideDuplicateReferenceBox]
+    public class FloatStat : IStat<float>
     {
-        this.baseValue = baseValue;
-    }
+        public float baseValue;
+        public List<StatModifier<float>> modifiers = new();
 
-    public StatModifier<float> AddModifier(StatModifier<float> mod)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing == null)
+        public float cachedValue;
+        bool isDirty = true;
+        public FloatStat(float baseValue)
         {
-            modifiers.Add(mod);
+            this.baseValue = baseValue;
         }
 
-        isDirty = true;
-        return mod;
-    }
-    public StatModifier<float> AddModifier(float value, StatModifierType type)
-    {
-        var mod = new StatModifier<float>(value, type);
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing == null)
+        public StatModifier<float> AddModifier(StatModifier<float> mod)
         {
-            modifiers.Add(mod);
-        }
+            var existing = modifiers.Find(m => m == mod);
 
-        isDirty = true;
-        return mod;
-    }
-    public void RemoveModifier(StatModifier<float> mod)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing != null)
-        {
-            modifiers.Remove(mod);
-
-            isDirty = true;
-        }
-    }
-    void IStat.RemoveModifier(StatModifierBase mod)
-    {
-        RemoveModifier(mod as StatModifier<float>);
-    }
-    public void ClearModifier(StatModifier<float> mod)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing != null)
-        {
-            modifiers.Remove(existing);
-
-            isDirty = true;
-
-        }
-    }
-    public void UpdateModifier(StatModifier<float> mod, float value)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing != null)
-        {
-            existing.value = value;
-
-            isDirty = true;
-        }
-    }
-
-    public float Value
-    {
-        get
-        {
-            if (!isDirty)
-                return cachedValue;
-
-            float flatSum = 0;
-            float additiveMultSum = 0;
-            float multiplicativeMultProduct = 1f;
-            float setValue = 0;
-            bool hasSet = false;
-
-            if (modifiers.Count != 0)
+            if (existing == null)
             {
-                foreach (var mod in modifiers)
+                modifiers.Add(mod);
+            }
+
+            isDirty = true;
+            return mod;
+        }
+        public StatModifier<float> AddModifier(float value, StatModifierType type)
+        {
+            var mod = new StatModifier<float>(value, type);
+            var existing = modifiers.Find(m => m == mod);
+
+            if (existing == null)
+            {
+                modifiers.Add(mod);
+            }
+
+            isDirty = true;
+            return mod;
+        }
+        public void RemoveModifier(StatModifier<float> mod)
+        {
+            var existing = modifiers.Find(m => m == mod);
+
+            if (existing != null)
+            {
+                modifiers.Remove(mod);
+
+                isDirty = true;
+            }
+        }
+        void IStat.RemoveModifier(StatModifierBase mod)
+        {
+            RemoveModifier(mod as StatModifier<float>);
+        }
+        public void ClearModifier(StatModifier<float> mod)
+        {
+            var existing = modifiers.Find(m => m == mod);
+
+            if (existing != null)
+            {
+                modifiers.Remove(existing);
+
+                isDirty = true;
+
+            }
+        }
+        public void UpdateModifier(StatModifier<float> mod, float value)
+        {
+            var existing = modifiers.Find(m => m == mod);
+
+            if (existing != null)
+            {
+                existing.value = value;
+
+                isDirty = true;
+            }
+        }
+
+        public float Value
+        {
+            get
+            {
+                if (!isDirty)
+                    return cachedValue;
+
+                float flatSum = 0;
+                float additiveMultSum = 0;
+                float multiplicativeMultProduct = 1f;
+                float setValue = 0;
+                bool hasSet = false;
+
+                if (modifiers.Count != 0)
                 {
-                    switch (mod.type)
+                    foreach (var mod in modifiers)
                     {
-                        case StatModifierType.Flat:
-                            flatSum += mod.value;
-                            break;
+                        switch (mod.type)
+                        {
+                            case StatModifierType.Flat:
+                                flatSum += mod.value;
+                                break;
 
-                        case StatModifierType.AdditiveMultiplier:
-                            additiveMultSum += mod.value;
-                            break;
+                            case StatModifierType.AdditiveMultiplier:
+                                additiveMultSum += mod.value;
+                                break;
 
-                        case StatModifierType.MultiplicativeMultiplier:
-                            multiplicativeMultProduct *= mod.value;
-                            break;
-                        case StatModifierType.Set:
-                            setValue = mod.value;
-                            hasSet = true;
-                            break;
+                            case StatModifierType.MultiplicativeMultiplier:
+                                multiplicativeMultProduct *= mod.value;
+                                break;
+                            case StatModifierType.Set:
+                                setValue = mod.value;
+                                hasSet = true;
+                                break;
+                        }
                     }
                 }
+                float baseToUse = hasSet ? setValue : baseValue;
+
+                cachedValue = (baseToUse + flatSum) * (1f + additiveMultSum) * multiplicativeMultProduct;
+                isDirty = false;
+
+                return cachedValue;
             }
-            float baseToUse = hasSet ? setValue : baseValue;
-
-            cachedValue = (baseToUse + flatSum) * (1f + additiveMultSum) * multiplicativeMultProduct;
-            isDirty = false;
-
-            return cachedValue;
         }
+        public int FloorValue => Mathf.FloorToInt(Value);
+        public int IntValue => Mathf.RoundToInt(Value);
+        public int CeilValue => Mathf.CeilToInt(Value);
     }
-    public int FloorValue => Mathf.FloorToInt(Value);
-    public int IntValue => Mathf.RoundToInt(Value);
-    public int CeilValue => Mathf.CeilToInt(Value);
-}
-public enum StatModifierType
-{
-    Flat, //+5
-    AdditiveMultiplier, //+20%
-    MultiplicativeMultiplier, //*2
-    Set //=2
+    public enum StatModifierType
+    {
+        Flat, //+5
+        AdditiveMultiplier, //+20%
+        MultiplicativeMultiplier, //*2
+        Set //=2
+    }
 }
--- Runtime/Stats/VectorStat.cs
+++ Runtime/Stats/VectorStat.cs
@@ -3,176 +3,180 @@ using System.Collections.Generic;
 using Opsive.Shared.Utility;
 using Sirenix.OdinInspector;
 using UnityEngine;
-[Serializable]
-[InlineProperty]
-[HideDuplicateReferenceBox]
-public class VectorStat : IStat<Vector3>
+namespace StatSystem
 {
-    public Vector3 baseValue;
-    public List<StatModifier<Vector3>> modifiers { get; } = new();
-
-    public Vector3 cachedValue;
-    bool isDirty = true;
-    public VectorStat(Vector3 baseValue) 
+    [Serializable]
+    [InlineProperty]
+    [HideDuplicateReferenceBox]
+    public class VectorStat : IStat<Vector3>
     {
-        this.baseValue = baseValue;
-        isDirty = true;
-    }
+        public Vector3 baseValue;
+        public List<StatModifier<Vector3>> modifiers { get; } = new();
 
-    public StatModifier<Vector3> AddModifier(StatModifier<Vector3> mod)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing == null)
+        public Vector3 cachedValue;
+        bool isDirty = true;
+        public VectorStat(Vector3 baseValue)
         {
-            modifiers.Add(mod);
-        }
-
-        isDirty = true;
-        return mod;
-    }
-    public StatModifier<Vector3> AddModifier(Vector3 value, StatModifierType type)
-    {
-        var mod = new StatModifier<Vector3>(value, type);
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing == null)
-        {
-            modifiers.Add(mod);
-        }
-
-        isDirty = true;
-        return mod;
-    }
-    public void RemoveModifier(StatModifier<Vector3> mod)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing != null)
-        {
-            modifiers.Remove(mod);
-
-            isDirty = true;
-
-        }
-    }
-    void IStat.RemoveModifier(StatModifierBase mod)
-    {
-        RemoveModifier(mod as StatModifier<Vector3>);
-    }
-    public void ClearModifier(StatModifier<Vector3> mod)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing != null)
-        {
-            modifiers.Remove(existing);
-
-            isDirty = true;
-
-        }
-    }
-    public void UpdateModifier(StatModifier<Vector3> mod, Vector3 value)
-    {
-        var existing = modifiers.Find(m => m == mod);
-
-        if (existing != null)
-        {
-            existing.value = value;
-
+            this.baseValue = baseValue;
             isDirty = true;
         }
-    }
-    public Vector3 Value
-    {
-        get
+
+        public StatModifier<Vector3> AddModifier(StatModifier<Vector3> mod)
         {
-            if (!isDirty)
-                return cachedValue;
+            var existing = modifiers.Find(m => m == mod);
 
-            Vector3 flatSum = Vector3.zero;
-            Vector3 additiveMultSum = Vector3.zero;
-            Vector3 multiplicativeMultProduct = Vector3.one;
-            Vector3 setValue = Vector3.zero;
-            bool hasSet = false;
-
-            if (modifiers.Count != 0)
+            if (existing == null)
             {
-                foreach (var mod in modifiers)
-                {
-                    switch (mod.type)
-                    {
-                        case StatModifierType.Flat:
-                            flatSum += mod.value;
-                            break;
-
-                        case StatModifierType.AdditiveMultiplier:
-                            additiveMultSum += mod.value;
-                            break;
-
-                        case StatModifierType.MultiplicativeMultiplier:
-                            multiplicativeMultProduct.x *= mod.value.x;
-                            multiplicativeMultProduct.y *= mod.value.y;
-                            multiplicativeMultProduct.z *= mod.value.z;
-                            break;
-                        case StatModifierType.Set:
-                            if (!hasSet)
-                            {
-                                setValue = baseValue;
-                                hasSet = true;
-                            }
-
-                            if (mod.value.x != baseValue.x) setValue.x = mod.value.x;
-                            if (mod.value.y != baseValue.y) setValue.y = mod.value.y;
-                            if (mod.value.z != baseValue.z) setValue.z = mod.value.z;
-                            break;
-                    }
-                }
+                modifiers.Add(mod);
             }
 
-            Vector3 baseToUse = hasSet ? setValue : baseValue;
+            isDirty = true;
+            return mod;
+        }
+        public StatModifier<Vector3> AddModifier(Vector3 value, StatModifierType type)
+        {
+            var mod = new StatModifier<Vector3>(value, type);
+            var existing = modifiers.Find(m => m == mod);
 
-            cachedValue = Vector3.Scale(baseToUse + flatSum, (Vector3.one + additiveMultSum));
+            if (existing == null)
+            {
+                modifiers.Add(mod);
+            }
 
-            cachedValue = Vector3.Scale(cachedValue, multiplicativeMultProduct);
-            isDirty = false;
+            isDirty = true;
+            return mod;
+        }
+        public void RemoveModifier(StatModifier<Vector3> mod)
+        {
+            var existing = modifiers.Find(m => m == mod);
 
-            return cachedValue;
+            if (existing != null)
+            {
+                modifiers.Remove(mod);
+
+                isDirty = true;
+
+            }
         }
-    }
-    public float x
-    {
-        get
+        void IStat.RemoveModifier(StatModifierBase mod)
         {
-            return Value.x;
+            RemoveModifier(mod as StatModifier<Vector3>);
         }
-    }
-    public float y
-    {
-        get
+        public void ClearModifier(StatModifier<Vector3> mod)
         {
-            return Value.y;
+            var existing = modifiers.Find(m => m == mod);
+
+            if (existing != null)
+            {
+                modifiers.Remove(existing);
+
+                isDirty = true;
+
+            }
         }
-    }
-    public float z
-    {
-        get
+        public void UpdateModifier(StatModifier<Vector3> mod, Vector3 value)
         {
-            return Value.z;
+            var existing = modifiers.Find(m => m == mod);
+
+            if (existing != null)
+            {
+                existing.value = value;
+
+                isDirty = true;
+            }
         }
-    }
-    public float magnitude
-    {
-        get
+        public Vector3 Value
         {
-            return Value.magnitude;
+            get
+            {
+                if (!isDirty)
+                    return cachedValue;
+
+                Vector3 flatSum = Vector3.zero;
+                Vector3 additiveMultSum = Vector3.zero;
+                Vector3 multiplicativeMultProduct = Vector3.one;
+                Vector3 setValue = Vector3.zero;
+                bool hasSet = false;
+
+                if (modifiers.Count != 0)
+                {
+                    foreach (var mod in modifiers)
+                    {
+                        switch (mod.type)
+                        {
+                            case StatModifierType.Flat:
+                                flatSum += mod.value;
+                                break;
+
+                            case StatModifierType.AdditiveMultiplier:
+                                additiveMultSum += mod.value;
+                                break;
+
+                            case StatModifierType.MultiplicativeMultiplier:
+                                multiplicativeMultProduct.x *= mod.value.x;
+                                multiplicativeMultProduct.y *= mod.value.y;
+                                multiplicativeMultProduct.z *= mod.value.z;
+                                break;
+                            case StatModifierType.Set:
+                                if (!hasSet)
+                                {
+                                    setValue = baseValue;
+                                    hasSet = true;
+                                }
+
+                                if (mod.value.x != baseValue.x) setValue.x = mod.value.x;
+                                if (mod.value.y != baseValue.y) setValue.y = mod.value.y;
+                                if (mod.value.z != baseValue.z) setValue.z = mod.value.z;
+                                break;
+                        }
+                    }
+                }
+
+                Vector3 baseToUse = hasSet ? setValue : baseValue;
+
+                cachedValue = Vector3.Scale(baseToUse + flatSum, (Vector3.one + additiveMultSum));
+
+                cachedValue = Vector3.Scale(cachedValue, multiplicativeMultProduct);
+                isDirty = false;
+
+                return cachedValue;
+            }
         }
-    }
-    public Vector3 normalized
-    {
-        get
+        public float x
         {
-            return Value.normalized;
+            get
+            {
+                return Value.x;
+            }
+        }
+        public float y
+        {
+            get
+            {
+                return Value.y;
+            }
+        }
+        public float z
+        {
+            get
+            {
+                return Value.z;
+            }
+        }
+        public float magnitude
+        {
+            get
+            {
+                return Value.magnitude;
+            }
+        }
+        public Vector3 normalized
+        {
+            get
+            {
+                return Value.normalized;
+            }
         }
     }
+
 }
