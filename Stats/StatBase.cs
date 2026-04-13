6f015d5aea631dad53d59eb4cf6245157cd81697
Speedy345 <109481540+Speedy345YT@users.noreply.github.com>
2026-04-12 19:01:38 -0600

    Add files via upload

---
+++ Stats/StatBase.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+public interface IStat
+{
+    void RemoveModifier(StatModifierBase mod);
+}
+public interface IStat<T> : IStat
+{
+    public StatModifier<T> AddModifier(StatModifier<T> mod);
+    public StatModifier<T> AddModifier(T value, StatModifierType type);
+    void RemoveModifier(StatModifier<T> mod);
+    void ClearModifier(StatModifier<T> mod);
+    void UpdateModifier(StatModifier<T> mod, T value);
+
+    T Value { get; }
+}
+public abstract class StatModifierBase
+{
+    public StatModifierType type;
+}
+public class StatModifier<T> : StatModifierBase
+{
+    public T value;
+    public StatModifier(T value, StatModifierType type)
+    {
+        this.value = value;
+        this.type = type;
+    }
+}
+public static class TaskExtensions
+{
+    static object AfterTicks(this object m, int ticks)
+    {
+        return (object)m;
+    }
+}
